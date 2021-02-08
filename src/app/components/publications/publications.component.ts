6dd637689ea4c42009e12208d399f4a25625c207
Naruto2020 <guyfredy@gmail.com>
2021-02-08 19:30:52 +0100

    gestion des publications

--- src/app/components/publications/publications.component.ts
+++ src/app/components/publications/publications.component.ts
@@ -20,18 +20,24 @@ export class PublicationsComponent implements OnInit {
 
   images:string ;
   url1:string;
+  compteur = 0;
+
+  userFoundload;
+  userFoundName;
+  userFoundFol;
+  userFoundId;
+
+  aProp1:string;
+  aProp2:string;
+  aProp3:string;
+  aProp4:string;
+  aProp5:string;
+  majTof:string;
 
   listeImages:any;
   listeImagesPro:any;
   listesProfils:any;
   friend:any;
-
-  partage = new FormGroup({
-    message : new FormControl(""),
-    body : new FormControl(""),
-    date : new FormControl(""),
-    loadBy: new FormControl(""),
-  });
 
   addImagePro = new FormGroup({
     _id : new FormControl(""),
@@ -39,15 +45,73 @@ export class PublicationsComponent implements OnInit {
     loadBy : new FormControl("")
   });
 
+  partage = new FormGroup({
+    message : new FormControl(""),
+    postId : new FormControl(""),
+    date : new FormControl(""),
+    loadBy: new FormControl(""),
+  });
+
   constructor(private bailService:BailService, private route: Router, private router : ActivatedRoute) { }
 
   ngOnInit(): void {
     this.userDisplayName = localStorage.getItem("loggedUser");
+    // on accède a la liste des utilisateurs en vu de récupérer l'id du post
+    this.bailService.showProfils().subscribe((res) =>{
+      //console.log(">>>",res);
+      this.listesProfils = res;
+      // on transforme le l'objet en tableau key/value
+      let newR = Object.keys(res).map(function(cle) {
+        return [Number(cle), res[cle]];
+      });
+      //console.log("new ...",newR);
+      //console.log(Object.entries(res));
+      for(let i=0; i< newR.length; i++){
+        let tabA = newR[i][1].followers;
+        let tab1 = newR[i][1].followings;
+        let tabId = newR[i][1]._id;
+        this.userFoundload = newR[i][1].username;
+        if(this.userDisplayName === this.userFoundload){
+          this.userFoundName = tabA;
+          this.userFoundFol = tab1;
+          this.userFoundId = tabId;
+          for(let j=0; j<tabA.length;j++){
+            this.compteur += 1;
+            for(let k=0; k<tab1.length; k++){
+              if(tab1[k] === tabA[j]){
+                this.compteur -=1;
+                
+              }
+            }
+          }
+
+        }
+        //console.log("yooo",newR[i]);
+        let tab = newR[i];
+        for(let j=0; j<tab.length;j++){
+          //console.log(tab[j].username);
+          let obj = tab[j];
+          //let objId = tab[j]._id;
+          if(obj.username === localStorage.getItem("loggedUser")){
+            this.aProp1 = obj.genre;
+            this.aProp2 = obj.age;
+            this.aProp3 = obj.preferences;
+            this.aProp4 = obj.presentation;
+            this.aProp5 = obj.amis;
+            this.majTof = obj._id;
+
+            //console.log("lolitapaz",this.aProp5);
+
+          }
+        }
+      }  
+
+    });
   }
 
   poster(){
     this.bailService.publierMess(this.partage.value).subscribe(res =>{
-      console.log(res)
+      console.log("staus",res)
       this.partage.reset({});
       return res;
     });
